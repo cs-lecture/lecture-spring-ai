002be2e58048f34bbfc584a50293df3c4d0ff607
xmin <liminggangrs@gmail.com>
2025-01-20 18:21:24 +0800

    init

---
+++ spring/src/main/java/com/xmin/lecture/mutimodality/MultiModalityController.java
@@ -0,0 +1,32 @@
+package com.xmin.lecture.mutimodality;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.model.Media;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MultiModalityController {
+
+    final OllamaChatModel chatModel;
+
+    @GetMapping("/ai/chatWithPic")
+    public String chatWithPic() {
+        ClassPathResource imageData = new ClassPathResource("/cat.png");
+        Message userMessage = new UserMessage("请用中文描述一下这张图片里面有什么东西？",
+                List.of(new Media(MimeTypeUtils.IMAGE_PNG, imageData)));
+        return this.chatModel.call(new Prompt(List.of(userMessage), ChatOptions.builder().model(OllamaModel.LLAVA.getName()).build()))
+                .getResult().getOutput().getContent();
+    }
+}
